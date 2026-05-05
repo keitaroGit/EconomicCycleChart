7dbb639bfa8875315c8f15b3901b079745f9e9fc
keitaroGit <keitaro.yone5025@gmail.com>
2026-05-04 21:47:33 -0700

    Fix PORT default to 5000 for Railway deployment

--- app.py
+++ app.py
@@ -294,5 +294,5 @@ def economic_phase():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
